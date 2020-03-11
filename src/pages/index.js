e6ed91c0511bdcad288ee69d12e059a4ed025094
Sarah Paz <sarah.paz@gmail.com>
2020-03-10 23:55:51 -0400

    remove footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import HomePage from "../components/Home/HomePage.component"
 import Navbar from "../components/Navbar/Navbar.component"
+import Footer from "../components/Footer/Footer.component"
 
 export default () => (
   <div>
