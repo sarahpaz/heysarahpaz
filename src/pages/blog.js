09d647d7b77ef7119d847eaa14a7c89eab071db0
Sarah Paz <sarah.paz@gmail.com>
2020-03-10 23:56:43 -0400

    create page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>Blog Post Title</h1>
+      <p>Blog post content</p>
+    </div>
+  )
+}
+
+export default BlogPage
---
+++ src/pages/experience.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ExperiencePage = () => {
+  return (
+    <div>
+      <h1>Experience</h1>
+    </div>
+  )
+}
+
+export default ExperiencePage
