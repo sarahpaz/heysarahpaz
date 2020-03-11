116cdd2b7eeb1e63ace1d2dafca9a4e5ced13408
Sarah Paz <sarah.paz@gmail.com>
2020-03-10 22:03:03 -0400

    add homepage and navbar components

---
+++ src/components/Home/HomePage.component.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./HomePage.styles.css"
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ src/components/Navbar/Navbar.component.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "./Navbar.styles.css"
+
+const Navbar = () => {
+  return <div>Navbar</div>
+}
+
+export default Navbar
