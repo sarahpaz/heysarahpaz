9152054fa4def368357062fb93c1aff6f644ffc4
Sarah Paz <sarah.paz@gmail.com>
2020-03-12 23:41:38 -0400

    install material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,15 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-theme-material-ui`,
+      options: {
+        stylesConfig: {
+          // disableAutoprefixing: true,
+          // disableMinification: true
+        },
+      },
+    },
+  ],
 }
