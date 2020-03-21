86144416f6737231d5cf9b28cd9f9494a6ff1fe4
Sarah Paz <sarah.paz@gmail.com>
2020-03-21 19:00:23 -0400

    add footer

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,12 @@
 import React from "react"
+import Footer from "../components/Footer/Footer.component"
 
 const BlogPage = () => {
   return (
     <div>
       <h1>Blog Post Title</h1>
       <p>Blog post content</p>
+      <Footer />
     </div>
   )
 }
