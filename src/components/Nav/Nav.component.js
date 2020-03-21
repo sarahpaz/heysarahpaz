eebf9e4788528599bb956a436f982e5a28369349
Sarah Paz <sarah.paz@gmail.com>
2020-03-21 19:30:36 -0400

    add nav tag and list items

--- src/components/Nav/Nav.component.js
+++ src/components/Nav/Nav.component.js
@@ -1,26 +1,16 @@
 import React from "react"
 import "./Nav.styles.css"
-import { AppBar } from "@material-ui/core"
-import { IconButton } from "@material-ui/core"
-import { Toolbar } from "@material-ui/core"
-import { Typography } from "@material-ui/core"
 
 const Nav = () => {
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-          ></IconButton>
-          <Typography variant="h6" color="inherit">
-            heysarahpaz
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Blog</li>
+        <li>Experience</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
   )
 }
 
