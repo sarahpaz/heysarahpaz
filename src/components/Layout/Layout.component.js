b80f13d99efa63ba0136b416dffebb4cd6be0fba
Sarah Paz <sarah.paz@gmail.com>
2020-03-22 16:43:06 -0400

    import layoutStyles

--- src/components/Layout/Layout.component.js
+++ src/components/Layout/Layout.component.js
@@ -1,11 +1,11 @@
 import React from "react"
-import "./Layout.styles.scss"
+import layoutStyles from "./Layout.module.scss"
 import Header from "../Header/Header.component"
 import Footer from "../Footer/Footer.component"
 
 const Layout = props => {
   return (
-    <div>
+    <div className={layoutStyles.container}>
       <Header />
       {props.children}
       <Footer />
