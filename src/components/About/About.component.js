c6cfb305c3534302149f22784d8cbf8222a131f6
Sarah Paz <sarah.paz@gmail.com>
2020-03-21 21:40:04 -0400

    create component

--- src/components/About/About.component.js
+++ src/components/About/About.component.js
@@ -1,9 +1,15 @@
 import React from "react"
+import "./About.styles.css"
 
 const About = () => {
   return (
-    <div>
-      <p>ABOUT SECTION</p>
+    <div className="about-container">
+      <p>
+        Full Stack Web Developer with5+ years E-Commerce experienceand a
+        background in Marketing.Extremely motivated and alwayslearning.
+        Passionate problem solverand creative thinker ready to take onany
+        challenge.
+      </p>
     </div>
   )
 }
