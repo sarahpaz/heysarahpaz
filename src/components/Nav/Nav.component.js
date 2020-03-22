2d56186f72bba15b43932df2cceaee45f16b98dd
Sarah Paz <sarah.paz@gmail.com>
2020-03-22 16:43:55 -0400

    import AppBar, ToolBar, IconButton, Typography and Button

--- src/components/Nav/Nav.component.js
+++ src/components/Nav/Nav.component.js
@@ -1,27 +1,62 @@
 import React from "react"
-import "./Nav.styles.scss"
+// import navStyles from "./Nav.module.scss"
+import { makeStyles } from "@material-ui/core/styles"
 import { AnchorLink } from "gatsby-plugin-anchor-links"
+import AppBar from "@material-ui/core/AppBar"
+import ToolBar from "@material-ui/core/ToolBar"
+import IconButton from "@material-ui/core/IconButton"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    background: "#999",
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}))
 
 const Nav = () => {
+  const classes = useStyles()
   return (
-    <nav>
-      <ul>
-        <li>
+    <AppBar className={classes.root} position="inherit">
+      <ToolBar>
+        <IconButton
+          edge="start"
+          color="inherit"
+          className={classes.menuButton}
+          aria-label="menu"
+        ></IconButton>
+        <Typography variant="h6" className={classes.title}>
+          heysarahpaz.
+        </Typography>
+        <Button color="inherit">
           <AnchorLink to="/#about" tite="About">
             About
           </AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="/#blog">Blog</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="/#experience">Experience</AnchorLink>
-        </li>
-        <li>
-          <AnchorLink to="/#contact">Contact</AnchorLink>
-        </li>
-      </ul>
-    </nav>
+        </Button>
+        <Button color="inherit">
+          <AnchorLink to="/#blog" tite="Blog">
+            Blog
+          </AnchorLink>
+        </Button>
+        <Button color="inherit">
+          <AnchorLink to="/#experience" tite="Experience">
+            Experience
+          </AnchorLink>
+        </Button>
+        <Button color="inherit">
+          <AnchorLink to="/#contact" tite="Contact">
+            Contact
+          </AnchorLink>
+        </Button>
+      </ToolBar>
+    </AppBar>
   )
 }
 
