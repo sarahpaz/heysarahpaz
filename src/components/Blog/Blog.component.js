0034047c1b8006915ebb12d6fecabf5c2a22d48b
Sarah Paz <sarah.paz@gmail.com>
2020-03-21 21:41:12 -0400

    create component

---
+++ src/components/Blog/Blog.component.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Blog.styles.css"
+
+const Blog = () => {
+  return (
+    <div className="blog-container">
+      <h2>Title</h2>
+      <p>Description</p>
+      <h2>Title</h2>
+      <p>Description</p>
+      <h2>Title</h2>
+      <p>Description</p>
+    </div>
+  )
+}
+
+export default Blog
---
+++ src/components/Experience/Experience.component.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Experience.styles.css"
+
+const Experience = () => {
+  return (
+    <div className="experience-container">
+      <h2>Technical Skills</h2>
+      <h2>Projects</h2>
+      <h2>Education</h2>
+    </div>
+  )
+}
+
+export default Experience
