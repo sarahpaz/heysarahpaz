62f94098896c8bdc1799bc5686328333bba2fd13
Sarah Paz <sarah.paz@gmail.com>
2020-04-01 00:18:31 -0400

    import Container from material-ui

--- src/components/Blog/Blog.component.js
+++ src/components/Blog/Blog.component.js
@@ -1,15 +1,18 @@
 import React from "react"
 import "./Blog.styles.scss"
+import Container from "@material-ui/core/Container"
 
 const Blog = () => {
   return (
     <div id="blog" className="blog-container">
-      <h2>Title</h2>
-      <p>Description</p>
-      <h2>Title</h2>
-      <p>Description</p>
-      <h2>Title</h2>
-      <p>Description</p>
+      <Container>
+        <h2>Title</h2>
+        <p>Description</p>
+        <h2>Title</h2>
+        <p>Description</p>
+        <h2>Title</h2>
+        <p>Description</p>
+      </Container>
     </div>
   )
 }
