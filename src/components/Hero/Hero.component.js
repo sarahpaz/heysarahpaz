a3c8504a76565634f34667955faa5be68ab7f33f
Sarah Paz <sarah.paz@gmail.com>
2020-04-01 00:20:38 -0400

    import Container from material-ui

--- src/components/Hero/Hero.component.js
+++ src/components/Hero/Hero.component.js
@@ -1,18 +1,20 @@
 import React from "react"
 import "./Hero.styles.scss"
-
+import Container from "@material-ui/core/Container"
 const Hero = () => {
   return (
     <div className="hero-container">
-      <h1>
-        Hey, I'm Sarah Paz. <br />
-        Developer. Runner. Traveller.
-      </h1>
-      <p>
-        I’m a web developer with a background in marketing and e-commerce. I
-        have a passion for learning, improving the user experience, and finding
-        creative solutions to common problems.
-      </p>
+      <Container>
+        <h1>
+          Hey, I'm Sarah Paz. <br />
+          Developer. Runner. Traveller.
+        </h1>
+        <p>
+          I’m a web developer with a background in marketing and e-commerce. I
+          have a passion for learning, improving the user experience, and
+          finding creative solutions to common problems.
+        </p>
+      </Container>
     </div>
   )
 }
