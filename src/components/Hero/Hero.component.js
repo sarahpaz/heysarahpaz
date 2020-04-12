1af264aa03359b2f742fdd5540e5b80eed4deafb
Sarah Paz <sarah.paz@gmail.com>
2020-04-12 00:47:48 -0400

    remove intro and add display flex to container

--- src/components/Hero/Hero.component.js
+++ src/components/Hero/Hero.component.js
@@ -1,15 +1,12 @@
 import React from "react"
 import "./Hero.styles.scss"
-import Container from "@material-ui/core/Container"
 const Hero = () => {
   return (
     <div className="hero-container">
-      <Container>
-        <h1>
-          Hey, I'm Sarah Paz. <br />
-          Developer. Runner. Traveller.
-        </h1>
-      </Container>
+      <h1>
+        Hey, I'm Sarah Paz. <br />
+        Developer. Runner. Traveller.
+      </h1>
     </div>
   )
 }
