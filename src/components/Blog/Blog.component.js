bfa07a6d990640d8128410b9400f257e891c8850
Sarah Paz <sarah.paz@gmail.com>
2020-04-11 22:49:16 -0400

    remove container component and change div to fragment

--- src/components/Blog/Blog.component.js
+++ src/components/Blog/Blog.component.js
@@ -1,19 +1,16 @@
 import React from "react"
 import "./Blog.styles.scss"
-import Container from "@material-ui/core/Container"
 
 const Blog = () => {
   return (
-    <div id="blog" className="blog-container">
-      <Container>
-        <h2>Title</h2>
-        <p>Description</p>
-        <h2>Title</h2>
-        <p>Description</p>
-        <h2>Title</h2>
-        <p>Description</p>
-      </Container>
-    </div>
+    <>
+      <h2>Title</h2>
+      <p>Description</p>
+      <h2>Title</h2>
+      <p>Description</p>
+      <h2>Title</h2>
+      <p>Description</p>
+    </>
   )
 }
 
