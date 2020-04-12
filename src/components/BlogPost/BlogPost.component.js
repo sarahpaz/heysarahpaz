2554af72d83d8d2258f6a9db0d6df97184695351
Sarah Paz <sarah.paz@gmail.com>
2020-04-12 00:44:05 -0400

    create BlogPost component

---
+++ src/components/BlogPost/BlogPost.component.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./BlogPost.styles.scss"
+
+const BlogPost = () => {
+  return (
+    <>
+      <h1>Title</h1>
+      <p>Description</p>
+    </>
+  )
+}
+
+export default BlogPost
